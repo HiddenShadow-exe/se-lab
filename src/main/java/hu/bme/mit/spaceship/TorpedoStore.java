fa3ba23886d6b05f2e331ff51755d00b1fb0b73e
HiddenShadow-exe <strumpf.david721@gmail.com>
2024-10-10 13:14:20 +0200

    docs: added docs to varaiables

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -12,6 +12,7 @@ public class TorpedoStore {
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; //NOSONAR
 
+  // remaining number of torpedos in the store
   private int torpedoCount = 0;
 
   private Random generator = new Random();
